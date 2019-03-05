bd30917b033696fdd3d6b90b24189e07ea24c16c
hidenori akiyama <mturu1976@gmail.com>
2019-03-05 23:15:57 +0900

    company追加

--- init.el
+++ init.el
@@ -106,3 +106,16 @@
                         ;;(agenda . 5)
                         ;;(registers . 5)
                         ))
+;; company                        
+(use-package company
+    :init
+    (setq company-selection-wrap-around t)
+    :bind
+    (:map company-active-map
+        ("M-n" . nil)
+        ("M-p" . nil)
+        ("C-n" . company-select-next)
+        ("C-p" . company-select-previous)
+        ("C-h" . nil))
+    :config
+    (global-company-mode))
